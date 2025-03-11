1822a395e4f7461912f5d77e9b93553295507eff
Zombi.filin <zombi.filin.256@gmail.com>
2025-03-11 15:18:55 +0300

    Доработка downloader_advantech_com.py

--- downloader_advantech_com.py
+++ downloader_advantech_com.py
@@ -27,18 +27,22 @@ def get_html(url, aspx = False):
 opened_id = []
 download_list = []
 
-f = open('usb-drivers.org.txt', 'w')
+f = open('advantech.com.txt', 'w')
 for suf in alphabet_list:
-    print(f'suf > {suf}')
     id_list_src = get_html(f'https://apis-corp.advantech.com/api/v1/search/documents/support?q={suf}&lang=en&use_fuzzy=false&data_types=Driver&page_index=1&page_size=1000')
     id_list_json = json.loads(id_list_src)
-    
-    if int(id_list_json['count']) > 1000:
+    all_count = int(id_list_json['count'])
+
+    if all_count > 1000:
         breakpoint()
+
+    count = 0    
+
     for line_json in id_list_json['documents']:
+        count += 1
         page_id = line_json['id']
-        
-        print(f'page_id > {page_id}')
+
+        print(f'{suf} > {count}/{all_count} > {page_id}')
         
         if page_id in opened_id:
             continue
@@ -46,6 +50,7 @@ for suf in alphabet_list:
 
         page_src = get_html(f'https://www.advantech.com/en/support/details/driver?id={page_id}')
         page_scr_list = page_src.split('\n')
+
         for page_src_line in page_scr_list:
             file_id_finds = re.findall(file_id_regex, page_src_line)
             if len(file_id_finds) > 1:
