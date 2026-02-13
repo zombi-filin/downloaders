a7d8d32e357f10a5c181b85cccac4dfa71e7accc
Zombi.filin <zombi.filin.256@gmail.com>
2026-02-13 15:45:43 +0300

    Переделка drivers_icl_techno_ru.py

--- drivers_icl_techno_ru.py
+++ drivers_icl_techno_ru.py
@@ -10,15 +10,25 @@ print('SCAN BEGIN')
 nom = 12000
 while(nom < (110000)):
     for prefix in prefix_list:
-        url = f'https://drivers.icl-techno.ru/download/{nom}-{prefix}.zip     '
-        print(f'Try: {url}', end='\r')
-        request = urllib.request.Request(url, None, headers)
-        try:
-            response = urllib.request.urlopen(request)
-            url_list.append(url)
-            print(f'Find: {url}')
-        except:
-            pass
+        while_flag = True
+        try_count = 0
+        while (while_flag):
+            try_count += 1 
+            url = f'https://drivers.icl-techno.ru/download/{nom}-{prefix}.zip     '
+            print(f'Try #{try_count} {url}', end='\r')
+            request = urllib.request.Request(url, None, headers)
+            try:
+                response = urllib.request.urlopen(request)
+                url_list.append(url)
+                print(f'Find: {url}')
+                while_flag = False
+            except urllib.error.URLError as e:
+                if e.code==404:
+                    while_flag = False
+                else:
+                    pass
+            except Exception:
+                pass
     nom += 1
 
 print('SCAN COMPLETE')
